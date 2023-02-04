ea7005d298619a6e06fb2bff70d9c866a828f442
Hassanabbaass <hassan.abbaass@gmail.com>
2023-02-04 12:58:51 +0200

    get books

--- server/index.js
+++ server/index.js
@@ -6,9 +6,23 @@ const app = express();
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "PFH#23kqwr9",
+    password: "PFH#23kgrw9",
     database: "bookapp"
 })
+
+app.get("/", (req,res) => {
+    res.json("Hello, This is the backend")
+})
+
+app.get("/books", (req,res) => {
+    const q = "SELECT * FROM books"
+    db.query(q, (err,data)=> {
+        if (err) return res.json(err)
+        return res.json(data)
+    })
+})
+
+
 
 app.listen(8800, ()=>{
     console.log("Server Is Running")
