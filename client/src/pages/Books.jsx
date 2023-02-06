2e33ff7228acb64930bbe21e522056dc1b54f185
Hassanabbaass <hassan.abbaass@gmail.com>
2023-02-06 11:52:10 +0200

    fetching books from api

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+
+import { getBooks } from '../services/getBooks'; 
+
 
 const Books = () => {
+
+    const [books, setBooks] = useState([])
+
+    useEffect(() => {
+        getBooks().then((result)=>{
+            console.log(result.data);
+        }).catch((err) => {
+            console.log(err)
+        })
+    }, [])
+
   return (
     <div>Books</div>
   )
