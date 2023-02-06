62dc31a1941277edafdd7f32afa6e6a779e152e1
Hassanabbaass <hassan.abbaass@gmail.com>
2023-02-06 12:36:02 +0200

    add book api

--- client/src/pages/Add.jsx
+++ client/src/pages/Add.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+import { addBook } from '../services/addBook';
 
 const Add = () => {
+
+    const nav = useNavigate();
+
+    const [addedBook, setAddedBook] = useState({
+        title: "",
+        desc: "",
+        price: null,
+        cover: "",
+    })
+
+    const handleChange = (e) => {
+        setAddedBook((prev) => (
+            {...prev, [e.target.name]: e.target.value}
+        ))
+    }
+
+    const handleAddBook = (e) => {
+        e.preventDefault();
+        addBook(addedBook).then((result)=> {
+            console.log(result)
+            nav('/')
+        }).catch((error)=>{
+            console.log(error)
+        })
+    }
+
   return (
-    <div>Add</div>
+    <div className='form'>
+        <h1>Add New Book</h1>
+        <input type="text" placeholder='title' onChange={handleChange} name='title'/>
+        <input type="text" placeholder='desc' onChange={handleChange} name='desc'/>
+        <input type="number" placeholder='price' onChange={handleChange} name='price'/>
+        <input type="text" placeholder='cover' onChange={handleChange} name='cover'/>
+        <button onClick={handleAddBook}>Add</button>
+    </div>
   )
 }
 
